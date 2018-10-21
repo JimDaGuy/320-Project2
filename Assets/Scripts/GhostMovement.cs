e8993f85d071ed532fe65a786dc964ca21759232
James DiGrazia <jimdaguy@csh.rit.edu>
2018-10-20 22:26:47 -0400

    Setup navmesh for pathfinding, give ghost prefab player-tracking

--- Assets/Scripts/GhostMovement.cs
+++ Assets/Scripts/GhostMovement.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GhostMovement : MonoBehaviour {
     public GameObject targetPlayer;
+    public Vector3 destination;
+    public NavMeshAgent agent;
 
 	// Use this for initialization
 	void Start () {
-		
+        destination = targetPlayer.transform.position;
+        agent.SetDestination(targetPlayer.transform.position);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        destination = targetPlayer.transform.position;
+        agent.SetDestination(destination);
 	}
 }
